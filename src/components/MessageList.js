4953aeb21a9d5808b849c1ffb1008299cd28ba79
schmidt-aaron <aschmidt808@gmail.com>
2019-02-17 10:09:08 -0800

    added new room form + auto scroll

--- src/components/MessageList.js
+++ src/components/MessageList.js
@@ -1,10 +1,37 @@
 import React from "react";
+import ReactDom from "react-dom";
 import Message from "./Message";
 
 class MessageList extends React.Component {
+  // scrolls to the bottom of the message list
+  scrollToBottom() {
+    const scrollHeight = this.messageDiv.scrollHeight;
+    const height = this.messageDiv.clientHeight;
+    const maxScrollTop = scrollHeight - height;
+    this.messageDiv.scrollTop = maxScrollTop > 0 ? maxScrollTop : 0;
+  }
+
+  // used to toggle auto scroll to bottom depending on where user is in messages
+  UNSAFE_componentWillUpdate() {
+    this.shouldScrollToBottom =
+      this.messageDiv.scrollTop + this.messageDiv.clientHeight + 100 >=
+      this.messageDiv.scrollHeight;
+  }
+
+  componentDidUpdate() {
+    if (this.shouldScrollToBottom) {
+      this.scrollToBottom();
+    }
+  }
+
   render() {
     return (
-      <div className="message-list">
+      <div
+        className="message-list"
+        ref={div => {
+          this.messageDiv = div;
+        }}
+      >
         {this.props.messages.map((message, index) => {
           return (
             <Message
--- src/components/NewRoom.js
+++ src/components/NewRoom.js
@@ -3,8 +3,13 @@ import React from "react";
 class NewRoom extends React.Component {
   render() {
     return (
-      <div className="new-room">
-        <p>NewRoom</p>
+      <div className="new-room-form">
+        <form>
+          <input type="text" placeholder="NewRoomForm" required />
+          <button id="create-room-form" type="submit">
+            +
+          </button>
+        </form>
       </div>
     );
   }
